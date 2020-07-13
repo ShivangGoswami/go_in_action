e341627a8d97c0bbbb57472d7171de7d7f35b203
ShivangGoswami <shivang.goswami@infosys.com>
2020-07-13 23:55:04 +0530

    go in action completed

---
+++ chapter9/listing17/handlers/handlers_example_test.go
@@ -0,0 +1,27 @@
+package handlers_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+)
+
+func ExampleSendJSON() {
+	r, _ := http.NewRequest("GET", "/sendjson", nil)
+	w := httptest.NewRecorder()
+	http.DefaultServeMux.ServeHTTP(w, r)
+
+	var u struct {
+		Name  string
+		Email string
+	}
+	if err := json.NewDecoder(w.Body).Decode(&u); err != nil {
+		log.Println("ERROR:", err)
+	}
+
+	fmt.Println(u)
+	// Output:
+	// {Bill bill@ardanstudios.com}
+}
